a7d0709da407834958d68763c7f405a3d6cae3b4
odin <odin>
2003-01-09 19:49:06 +0000

    *** empty log message ***

--- src/kernel/linux.c
+++ src/kernel/linux.c
@@ -294,7 +294,6 @@ int masq(	int sock,
 		in_port_t masq_lport;
 		in_port_t masq_fport;
 		char user[MAX_ULEN];
-		in_addr_t localn;
 		in_addr_t remoten;
 		in_addr_t localm;
 		in_addr_t remotem;
@@ -321,6 +320,7 @@ int masq(	int sock,
 			int nl1, nl2, nl3, nl4, nr1, nr2, nr3, nr4;
 			u_int32_t nport_temp;
 			u_int32_t mport_temp;
+			in_addr_t localn;
 			u_int32_t masq_lport_temp;
 			u_int32_t masq_fport_temp;
 
@@ -344,10 +344,10 @@ int masq(	int sock,
 
 			localn = nl1 << 24 | nl2 << 16 | nl3 << 8 | nl4;
 			remoten = nr1 << 24 | nr2 << 16 | nr3 << 8 | nr4;
-		}
 
-		if (remotem != localn)
-			remotem = localn;       
+			if (remotem != localn)
+				remotem = localn;       
+		}
 
 		if (strcasecmp(proto, "tcp"))
 			continue;
